44f1509ecbf342a1b8dfdde1f8e81782e9460e79
Jacob Steimer <jacobsteimer@gmail.com>
2023-12-13 11:43:18 -0600

    Hopefully final updates before submitting. Added more description and recreated the pdf.

--- CodeToFollow.R
+++ CodeToFollow.R
@@ -1,3 +1,6 @@
+# In this code, we will analyze the relationship between bankruptcy rates in U.S. ZIP codes 
+# and various demographic variables pulled from the U.S. Census Bureau. 
+
 # Install all needed packages
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
@@ -20,28 +23,22 @@ library(lubridate)
 options(timeout = 120)
 
 # This code will automatically download and load the two necessary datasets
-
-Brup_file_url <- "https://github.com/jacobsteimer/USBankruptcyZIP/blob/main/cpbankdec22.zip"
-Brup_local_zip <- "cpbankdec22.zip"
-download.file(Brup_file_url, Brup_local_zip)
-unzip(Brup_local_zip, exdir = "data_directory")
-Brup_data <- read_sas("data_directory/cpbankdec22.sas7bdat")
+# The data, originally gathered from the Federal Judicial Center and the Census Bureau, has been compiled on GitHub for ease of use.
+Brup_file_url <- "https://github.com/jacobsteimer/USBankruptcyZIP/raw/main/BankruptcyData2022.zip"
+local_zip_file <- basename(Brup_file_url)
+download.file(Brup_file_url, local_zip_file, mode = "wb")
+unzip(local_zip_file)
+Brup_data <- readr::read_csv("BankruptcyData2022.csv")
 head(Brup_data)
 Census_file_url <- "https://github.com/jacobsteimer/USBankruptcyZIP/raw/main/CensusData.csv"
 census_data <- readr::read_csv(Census_file_url)
 
-#Getting started, let's ensure there are no duplicate cases
-Brup_No_Dup <- Brup_data %>% distinct(CASEKEY, .keep_all = TRUE)
-
-#This dataset includes all cases that were either filed in 2022 or were still open then. Let's cut it down to just cases filed that year.
-Brup_No_Dup_2022 <- Brup_No_Dup %>% filter((year(FILEDATE) == 2022))
-
-#Then, let's clean up the ZIP column
-Brup_No_Dup_2022$GEOID <- substr(Brup_No_Dup_2022$D1ZIP, 1, 5)
+#Getting started, let's clean up the ZIP column
+Brup_data$GEOID <- substr(Brup_data$D1ZIP, 1, 5)
 
 #Then, let's count up the number of bankruptcies in each ZIP and the number of Chapter 13 Bankruptcies in each ZIP
-BrupZIPcounts <- Brup_No_Dup_2022 %>% group_by(GEOID) %>% summarize(Bankruptcies = n())
-ThirteenZIPcounts <- Brup_No_Dup_2022 %>% filter(ORGFLCHP == 13) %>% group_by(GEOID) %>% summarize(Thirteens = n())
+BrupZIPcounts <- Brup_data %>% group_by(GEOID) %>% summarize(Bankruptcies = n())
+ThirteenZIPcounts <- Brup_data %>% filter(ORGFLCHP == 13) %>% group_by(GEOID) %>% summarize(Thirteens = n())
 BrupAndThirteenZIP <- inner_join(BrupZIPcounts,ThirteenZIPcounts, by = "GEOID")
 
 #Let's also calculate the percentage of Bankruptcies that are Chapter 13
@@ -55,6 +52,8 @@ Brup_w_Census <- Brup_w_Census %>% mutate(BrupPerThou = Bankruptcies/Population*
 
 # Also, let's divide MedIncome by 10,000. A difference in $10,000 between ZIP codes is a more understandable change than a difference of $1.
 Brup_w_Census$MedIncome <- Brup_w_Census$MedIncome/10000 
+# And let's remove ZIPs with populations of less than 250, as these could skew the per-thousand measures
+Brup_w_Census <- Brup_w_Census %>% filter(Population > 250)
 
 #Before doing any analysis, let's split it into a holdout set, a testing set and a training set
 set.seed(1, sample.kind="Rounding")
@@ -66,11 +65,79 @@ test_index2 <- createDataPartition(y = BrupCensusUse$Bankruptcies, times = 1, p 
 BrupCensusTrain <- BrupCensusUse[-test_index2,]
 BrupCensusTest <- BrupCensusUse[test_index2,]
 
-#To help my laptop run better, I'm going to go ahead and remove "Brup_data" and "Brup_No_Dup"
-rm(Brup_data)
-rm(Brup_No_Dup)
+# Now, let's do some data exploration
+summary(BrupCensusTrain)
+# The variation in bankruptcy filings is obvious, as the 1st quartile is 4, the median is 11, the 3rd quartile is 27 and the maximum is 446.
+# There are clearly some tremendous outliers for most of these variables. 
 
-#Now, let's analyze!
+# Let's explore further
+hist(BrupCensusTrain$Bankruptcies, main = "Distribution of total bankruptcies", xlab = "Bankruptcies")
+hist(BrupCensusTrain$BrupPerThou, main = "Distribution of Bankruptcies per 1000 Residents", xlab = "Bankruptcies per 1000 Residents")
+hist(BrupCensusTrain$ThirteenPerThou, main = "Distribution of Chapter 13 filings per 1000 Residents", xlab = "Chapter 13 filings per 1000 Residents")
+hist(BrupCensusTrain$ThirteenPerc, main = "Distribution of types of bankruptcy used", xlab = "Percentage of personal bankruptcy filings that are Chapter 13")
+# The fourth histogram, showing the percentage of filings that are Chapter 13 in different ZIPs is quite useful.
+# The other histograms primarily make clear the presence of outliers. Let's use logarithm to further explore.
+
+log_brupperthou <- log(Brup_w_Census$BrupPerThou)
+hist(log_brupperthou, main = "Distribution of Bankruptcies per 1000 Residents", xlab = "Bankruptcies per 1000 Residents")
+log_bankruptcies <- log(Brup_w_Census$Bankruptcies)
+hist(log_bankruptcies, main = "Distribution of total bankruptcies", xlab = "Bankruptcies per 1000 Residents")
+log_thirteenperthou <- log(BrupCensusTrain$ThirteenPerThou)
+hist(log_thirteenperthou, main = "Distribution of Chapter 13 filings per 1000 Residents", xlab = "Chapter 13 filings per 1000 Residents")
+
+# While the variation of total bankruptcies is pretty wide and uneven, the distribution of bankruptcies per thousand residents and Chapter 13 filings per thousand residents appear to roughly follow the normal distribution
+
+# To explore relationships between variables, let's create scatter plots for relevant pairs of variables.
+par(mfrow=c(2, 2))
+plot(BrupCensusTrain$BlackPercent, BrupCensusTrain$BrupPerThou, main = "Bankruptcies vs. Black Percentage", xlab = "Black Percentage", ylab = "Bankruptcies per 1000 Residents")
+plot(BrupCensusTrain$MedIncome, BrupCensusTrain$BrupPerThou, main = "Bankruptcies vs. Median Income", xlab = "Median Income", ylab = "Bankruptcies per 1000 Residents")
+plot(BrupCensusTrain$HomeOwnRate, BrupCensusTrain$BrupPerThou, main = "Bankruptcies vs. Homeownership Rate", xlab = "Homeownership Rate", ylab = "Bankruptcies per 1000 Residents")
+plot(BrupCensusTrain$BachPercent, BrupCensusTrain$BrupPerThou, main = "Bankruptcies vs. College Education Percentage", xlab = "College Education Percentage", ylab = "Bankruptcies per 1000 Residents")
+# These don't seem to show much other than the outliers. Let's adjust.
+remove_outliers <- function(data, variable) {
+  Q1 <- quantile(data[[variable]], 0.25, na.rm = TRUE)
+  Q3 <- quantile(data[[variable]], 0.75, na.rm = TRUE)
+  IQR <- Q3 - Q1
+  data <- data[data[[variable]] >= (Q1 - 1.5 * IQR) & data[[variable]] <= (Q3 + 1.5 * IQR), ]
+  return(data)
+}
+BrupCensusTrainNoOutliers <- remove_outliers(BrupCensusTrain, "BlackPercent")
+BrupCensusTrainNoOutliers <- remove_outliers(BrupCensusTrainNoOutliers, "MedIncome")
+BrupCensusTrainNoOutliers <- remove_outliers(BrupCensusTrainNoOutliers, "HomeOwnRate")
+BrupCensusTrainNoOutliers <- remove_outliers(BrupCensusTrainNoOutliers, "BachPercent")
+BrupCensusTrainNoOutliers <- remove_outliers(BrupCensusTrainNoOutliers, "BrupPerThou")
+
+ggplot(BrupCensusTrainNoOutliers, aes(x = BlackPercent, y = BrupPerThou)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(title = "Bankruptcies vs. Black Percentage",
+       x = "Black Percentage",
+       y = "Bankruptcies per 1000 Residents")
+
+ggplot(BrupCensusTrainNoOutliers, aes(x = MedIncome, y = BrupPerThou)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(title = "Bankruptcies vs. Median Income",
+       x = "Median Income",
+       y = "Bankruptcies per 1000 Residents")
+
+ggplot(BrupCensusTrainNoOutliers, aes(x = HomeOwnRate, y = BrupPerThou)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(title = "Bankruptcies vs. Homeownership Rate",
+       x = "Homeownership Rate",
+       y = "Bankruptcies per 1000 Residents")
+
+ggplot(BrupCensusTrainNoOutliers, aes(x = BachPercent, y = BrupPerThou)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(title = "Bankruptcies vs. College Education Percentage",
+       x = "College Education Percentage",
+       y = "Bankruptcies per 1000 Residents")
+# These help us visualize the relationships between bankruptcy and these select variables.
+
+
+#Now, let's start our analysis in full!
 #Let's try an lm with all six variables
 fitlm <- lm(BrupPerThou ~ BlackPercent + LabForceParticipation + HomeOwnRate + UnRate + MedIncome + BachPercent, data = BrupCensusTrain)
 tidy(fitlm)
@@ -173,35 +240,35 @@ HO_predictions_fitrf <- predict(fitrf, newdata = BrupCensusHoldoutAdjusted, na.a
 rmse_HO_fitrf <- RMSE(HO_predictions_fitrf, BrupCensusHoldoutAdjusted$BrupPerThou, na.rm = TRUE)
 # It turns out the model performed even better on the holdout set than the Test set. Nice!
 
-#Now that we've run our analysis using only the census data and the BrupZIPcounts, let's try to predict whether a bankruptcy is a Chapter 7 or Chapter 13, using more parts of the Bankruptcy dataset
-#First, let's pare it down a bit and filter it to just chapter 7s and chapter 13s
-Brup_Pared <- Brup_No_Dup_2022 %>% select(ORGFLCHP, TOTASSTS, REALPROP, PERSPROP, TOTLBLTS, SECURED, UNSECPR, UNSECNPR, TOTDBT, CNTMNTHI)
+# While the above accomplished our primary goals, it ignored most of the original Bankruptcy dataset
+# Let's see which variables in the original dataset are predictive of chapter type.
+# First, let's pare it down a bit and filter it to just chapter 7s and chapter 13s
+Brup_Pared <- Brup_data %>% select(ORGFLCHP, TOTASSTS, REALPROP, PERSPROP, TOTLBLTS, SECURED, UNSECPR, UNSECNPR, TOTDBT, CNTMNTHI)
 Brup_Pared <- Brup_Pared %>% filter(ORGFLCHP %in% c(7,13))
 #Now, let's make 7 vs. 13 a binary result
 Brup_Pared$Binary <- ifelse(Brup_Pared$ORGFLCHP == "13", 1, 0)
 #And let's get rid of NAs
 Brup_Pared_Adj <- na.omit(Brup_Pared)
 
-#Then, let's subset out a final holdout set, a testing set, a training set, and a small training set
+#Then, let's subset out a testing set, a training set, and a small training set
 set.seed(1, sample.kind="Rounding")
 test_index3 <- createDataPartition(y = Brup_Pared_Adj$ORGFLCHP, times = 1, p = 0.1, list = FALSE)
-Brup_Pared_Use <- Brup_Pared_Adj[-test_index3,]
-Brup_Pared_Holdout <- Brup_Pared_Adj[test_index3,]
+BrupParedTrain <- Brup_Pared_Adj[-test_index3,]
+BrupParedTest <- Brup_Pared_Adj[test_index3,]
 
-test_index4 <- createDataPartition(y = Brup_Pared_Use$ORGFLCHP, times = 1, p = 0.1, list = FALSE)
-BrupParedTrain <- Brup_Pared_Use[-test_index4,]
-BrupParedTest <- Brup_Pared_Use[test_index4,]
-
-test_index5 <- createDataPartition(y = Brup_Pared_Use$ORGFLCHP, times = 1, p = 0.1, list = FALSE)
-ParedSmallTrain <- BrupParedTrain[test_index5,]
+test_index4 <- createDataPartition(y = Brup_Pared_Adj$ORGFLCHP, times = 1, p = 0.1, list = FALSE)
+ParedSmallTrain <- BrupParedTrain[test_index4,]
 #Let's run an lm on the small version, to help us figure out which variables are important.
 fitlm7 <- lm(Binary ~ TOTASSTS + REALPROP + PERSPROP + TOTLBLTS + SECURED + UNSECPR + UNSECNPR + TOTDBT + CNTMNTHI, data = ParedSmallTrain, na.action = na.omit)
 tidy(fitlm7)
-#REALPROP, CNTMNTHI, and SECURED are by far the most predictive variables
+#REALPROP (real property), and SECURED (secured debt) are by far the most predictive variables, with CNTMNTHI (current monthly income) in third.
 
-#Let's go back and work the whole dataset
+#Let's go back and work the whole dataset, studying those three variables
 fitlm8 <- lm(Binary ~ REALPROP + SECURED + CNTMNTHI, data = BrupParedTrain, na.action = na.omit)
 tidy(fitlm8)
+# Interestingly, current monthly income rises to the top and real property loses most of its predictive power.
+
+# Now, let's see what our RMSE is.
 predictions_fitlm8 <- predict(fitlm8, newdata = BrupParedTest)
 rmse_fitlm8 <- RMSE(predictions_fitlm8, BrupParedTest$Binary, na.rm = TRUE)
 #That rmse is pretty great! About .49. 
